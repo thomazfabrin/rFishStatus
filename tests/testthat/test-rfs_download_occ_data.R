bcdd50c12864554590a7eeca25274133733be6df
thomazfabrin <thomazmcf@gmail.com>
2025-05-20 11:18:02 -0300

    Add tests for rfs_download_occ_data()

--- tests/testthat/test-rfs_download_occ_data.R
+++ tests/testthat/test-rfs_download_occ_data.R
@@ -11,6 +11,40 @@ test_that("rfs_download_occ_data handles invalid email", {
   )
 })
 
+test_that("rfs_download_occ_data handles user", {
+  expect_error(
+    rfs_download_occ_data(
+      gbif_user = 123,
+      gbif_pwd = "pwd",
+      gbif_email = "valid_email@email.com",
+      country = "Brazil",
+      state = "Paraná"
+    ),
+    regexp = "country, state, gbif_user, gbif_pwd, and gbif_email should be characters."
+  )
+})
+
+test_that("rfs_download_occ_data handles user polygon", {
+  temp_folder <- tempfile("occ_data_test2")
+  expect_no_error(
+    rfs_download_occ_data(
+      gbif_user = Sys.getenv("GBIF_USER"),
+      gbif_pwd = Sys.getenv("GBIF_PWD"),
+      gbif_email = Sys.getenv("GBIF_EMAIL"),
+      folder_name = temp_folder,
+      user_polygon = rnaturalearth::ne_states(
+        country = "Brazil",
+        returnclass = "sf"
+      ) |>
+        dplyr::filter(name == "Paraná"),
+      reverse_polygon = TRUE,
+      taxonkey = 5208140,
+      max_attempts = 6,
+      sleep_time = 20
+    )
+  )
+})
+
 test_that("rfs_download_occ_data handles non-numeric max_attempts and sleep_time", {
   expect_error(
     rfs_download_occ_data(
